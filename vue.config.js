0913ed3dbd7e6ba872320b4c59119c8de1b07436
Adri Candra Saputra Mangidi <adricandrasaputramangidi@gmail.com>
2023-07-03 20:54:15 +0800

    setting public path

--- vue.config.js
+++ vue.config.js
@@ -1,11 +1,11 @@
 const { defineConfig } = require('@vue/cli-service');
 module.exports = defineConfig({
-    transpileDependencies: true,
-    publicPath: process.env.NODE_ENV === 'production' ? '/store' : '/',
+  transpileDependencies: true,
+  publicPath: process.env.NODE_ENV === 'production' ? '/dist' : '/',
 
-    pluginOptions: {
-        vuetify: {
-            // https://github.com/vuetifyjs/vuetify-loader/tree/next/packages/vuetify-loader
-        },
+  pluginOptions: {
+    vuetify: {
+      // https://github.com/vuetifyjs/vuetify-loader/tree/next/packages/vuetify-loader
     },
+  },
 });
